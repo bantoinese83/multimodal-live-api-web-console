72a782483368aff79af0a34f4f2dd55ea70a5b59
bantoinese83 <b.antoine.se@gmail.com>
2025-01-23 21:43:00 -0500

    fixing style

--- src/components/video-player/VideoPlayer.tsx
+++ src/components/video-player/VideoPlayer.tsx
@@ -2,18 +2,30 @@ import React, { forwardRef } from "react";
 
 type VideoPlayerProps = {
   videoRef: React.RefObject<HTMLVideoElement>;
+  src?: string;
+  controls?: boolean;
+  muted?: boolean;
+  loop?: boolean;
+  className?: string;
 };
 
-const VideoPlayer: React.FC<VideoPlayerProps> = forwardRef(({ videoRef }) => {
-  return (
-    <div>
-      <video
-        ref={videoRef}
-        autoPlay
-        playsInline
-      />
-    </div>
-  );
-});
+const VideoPlayer: React.FC<VideoPlayerProps> = forwardRef(
+  ({ videoRef, src, controls = true, muted = false, loop = false, className = "" }, ref) => {
+    return (
+      <div className={className}>
+        <video
+          ref={videoRef}
+          src={src}
+          controls={controls}
+          muted={muted}
+          loop={loop}
+          autoPlay
+          playsInline
+          className="custom-video-player" // Add this class
+        />
+      </div>
+    );
+  }
+);
 
 export default VideoPlayer;
